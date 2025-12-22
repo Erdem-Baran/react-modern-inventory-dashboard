2b1864f4b7a9f294c0625c20310dcc059a288c39
Erdem-Baran <erdembaran638@gmail.com>
2025-12-22 07:33:34 +0300

    "addProduct api has been created"

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -2,6 +2,12 @@ import axiosClient from "./axiosClient";
 import type { Product } from "../types/product";
 
 export const getProducts = async (): Promise<Product[]> => {
-    const response = await axiosClient.get<Product[]>("/products")
-    return response.data;
-}
+  const response = await axiosClient.get<Product[]>("/products");
+  return response.data;
+};
+export const addProduct = async (
+  newProduct: Omit<Product, "id">
+): Promise<Product> => {
+  const response = await axiosClient.post<Product>("/product", newProduct);
+  return response.data;
+};
